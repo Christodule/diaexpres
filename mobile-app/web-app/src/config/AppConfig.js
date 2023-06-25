3d5f126458f1895196b8e4ae26505696675f897e
Christodule <zcedric121@gmail.com>
2023-06-25 17:19:46 -0600

    first commit

---
+++ mobile-app/web-app/src/config/AppConfig.js
@@ -0,0 +1,12 @@
+module.exports.AppConfig = {
+    app_name: 'diaexpres', 
+    app_description: 'Your App Description Here', 
+    app_identifier: 'com.diaexpres.com', 
+    ios_app_version: '1.0.0', 
+    android_app_version: 100, 
+    facebookAppId: '12345',
+    facebookClientToken: '5078130435533598',
+    expo_owner: 'christodule',
+    expo_slug: 'diaexpress-d601a',
+    expo_project_id: '4c57f657-ca20-41b1-888b-35e0ec45896b'
+}
